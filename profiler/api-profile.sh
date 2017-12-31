19e56825e93ee8099b5248c936987bd32234b16d
Kyle Rush <kdr213@gmail.com>
2017-12-30 18:14:50 -0800

    profiler checks for response code, testing different inputs with register-user

--- profiler/api-profile.sh
+++ profiler/api-profile.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 K=5;    
 HTTPSA='https://nkfpt8zca8.execute-api.us-west-2.amazonaws.com/prod/beta/register-user'    
+LOGFILE='./profile_'$(date +%F_%T)
 
-date +%M-%S-%N>res
+#Test for 200 response
+DATA=`bash register-user-postdata.sh`
+echo $'register-user\n\tresponse code:\t'`curl -so ./dev/null --write-out "%{http_code}" -d $DATA -H "Content-Type: application/JSON" $HTTPSA` | tee -a $LOGFILE 
+curl -so ./dev/null --write-out "%{http_code}" -d $DATA -H "Content-Type: application/JSON" $HTTPSA  | tee -a $LOGFILE
+
+#Latency test
 for (( c=1; c<=$K; c++ ))
 do
 	DATA=`bash register-user-postdata.sh`
-    curl -d $DATA -H "Content-Type: application/x-www-form-urlencoded" $HTTPSA
+    TIME=`curl -so ./dev/null --write-out "%{time_starttransfer}" -d $DATA -H "Content-Type: application/JSON" $HTTPSA`
+    echo $TIME | tee -a $LOGFILE
 done
-date +%M-%S-%N>>res
--- profiler/register-user-postdata.sh
+++ profiler/register-user-postdata.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
+INVALID=0
+# As long as there is at least one more argument, keep looping
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case "$key" in
+        # Also a flag type option. Will catch either -b or --bar
+        -i|--invalid)
+        INVALID=1
+        ;;
+    esac
+    # Shift after checking all the cases to get the next option
+    shift
+done
+
+if [ $INVALID -eq 1 ]
+then
+#Invalid input, pass too short
+echo '{"username":"testinvalid","password":"short","email":"kdr213@gmail.com","firstname":"Kyle","lastname":"R"}'
+else
 RANDSTRING=`bash ./rand-string.sh`
 echo '{"username":"'$RANDSTRING'","password":"1234567","email":"kdr213@gmail.com","firstname":"Kyle","lastname":"R"}'
+fi
